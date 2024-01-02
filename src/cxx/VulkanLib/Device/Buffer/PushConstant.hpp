71af4ce102b8fef9be926997369938867fb3949d
kgaft <you@example.com>
2024-01-03 01:53:28 +0300

    dynamic rendering tested for ofscreen and fix in push constant

--- src/cxx/VulkanLib/Device/Buffer/PushConstant.hpp
+++ src/cxx/VulkanLib/Device/Buffer/PushConstant.hpp
@@ -3,8 +3,27 @@
 //
 #pragma once
 
-class PushConstant {
+#include <vulkan/vulkan.hpp>
 
+class PushConstant {
+public:
+    PushConstant(size_t size, vk::PipelineLayout pipelineLayout) : size(size), layout(pipelineLayout){
+        data = calloc(1, size);
+    }
+private:
+    void* data;
+    size_t size;
+    vk::PipelineLayout layout;
+public:
+    void bind(vk::CommandBuffer cmd, vk::ShaderStageFlags bindPoint){
+        cmd.pushConstants(layout, bindPoint, 0, size, data);
+    }
+    void writeData(void* data, size_t writeSize){
+        if(writeSize>size){
+            throw std::runtime_error("Error: you cannot write data that size exceeds the push constant size");
+        }
+        memcpy(PushConstant::data, data, writeSize);
+    }
 };
 
 
