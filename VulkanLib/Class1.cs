e293437238462ec3b0b2499de4a8b453cbe6130c
kgaft <kgaft@github.com>
2024-10-19 06:14:54 +0300

    project setting up

---
+++ VulkanLib/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace VulkanLib;
+
+public class Class1
+{
+    public String message() => "Hello from VulkanLib!";
+}
---
+++ VulkanLibTesting/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using VulkanLib;
+Class1 test = new Class1();
+Console.WriteLine(test.message());
